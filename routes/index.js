999a850f4bbd431e54fc5fc7062f023559e972a8
Nicolai Safai <nicolaisafai@gmail.com>
2018-09-22 14:58:18 -0700

    added a new route, test

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { greeting: 'Hola', name: 'amigo', title: 'Rotten Potatoes' });
+});
+
+// create new route below
+router.get('/test', (req, res) => {
+  // render Handlebars template here...
+    res.render('test', { data: 'test' })
 });
 
 module.exports = router;

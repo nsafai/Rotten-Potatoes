efe8e26d732ec1f2b70e8315ba923e355931c82b
Nicolai Safai <nicolaisafai@gmail.com>
2018-09-23 00:06:02 -0700

    review cards with static data finished

--- routes/index.js
+++ routes/index.js
@@ -1,26 +1,38 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { greeting: 'Hola', name: 'amigo', title: 'Rotten Potatoes' });
-});
-
-
-let testArray = [
-  { data: 'Snickersnee' },
-  { data: 'Cattywampus' },
-  { data: 'Gardyloo' },
-  { data: 'Taradiddle' },
-  { data: 'Bumfuzzle' }
+let reviews = [
+  {
+    title: 'Wakanda forever',
+    movieName: 'Black Panther',
+    rating: 4,
+    body: 'Even soulless, Easter-egg hating film snobs like me can find a lot to admire in Black Panther.'
+  },
+  {
+    title: 'Welcome back Spielberg',
+    movieName: 'Ready Player One',
+    rating: 3,
+    body: "Spielberg wants us to drop the techno-gadgets and join hands, but it's the VR world that really juices him. He's the ultimate fanboy making a movie about the need to move beyond being a fan."
+  },
+  {
+    title: 'Fast, funny, and fantastic',
+    movieName: 'Rush Hour',
+    rating: 4,
+    body: "Rush Hour is a righteous title for an action comedy, and that's only the beginning."
+  },
+  {
+    title: 'DiCaprio gives the best performance of his career.',
+    movieName: 'The Wolf of Wall Street',
+    rating: 5,
+    body: "The result is a superb performance from DiCaprio as the swaggering, shouting bastard, and probably Scorsese's best film in 15 years."
+  }
 ];
 
-// create new route below
-router.get('/test', (req, res) => {
-  res.render('test', {
-    data: 'test',
-    test_items:testArray
-  })
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('reviews/index', { reviews: reviews});
 });
+
 
 module.exports = router;

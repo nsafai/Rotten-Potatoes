e78073b803665be5cde0e22ef3d6447eb58c458b
Nicolai Safai <nicolaisafai@gmail.com>
2018-09-23 00:36:02 -0700

    reading from DB

--- routes/index.js
+++ routes/index.js
@@ -4,39 +4,21 @@ var router = express.Router();
 // To use the Review model, we'll need to define it as a constant in our routes/index.js.
 const Review = require('../models/review');
 
-let reviews = [{
-        title: 'Wakanda forever',
-        movieName: 'Black Panther',
-        rating: 4,
-        body: 'Even soulless, Easter-egg hating film snobs like me can find a lot to admire in Black Panther.'
-    },
-    {
-        title: 'Welcome back Spielberg',
-        movieName: 'Ready Player One',
-        rating: 3,
-        body: "Spielberg wants us to drop the techno-gadgets and join hands, but it's the VR world that really juices him. He's the ultimate fanboy making a movie about the need to move beyond being a fan."
-    },
-    {
-        title: 'Fast, funny, and fantastic',
-        movieName: 'Rush Hour',
-        rating: 4,
-        body: "Rush Hour is a righteous title for an action comedy, and that's only the beginning."
-    },
-    {
-        title: 'DiCaprio gives the best performance of his career.',
-        movieName: 'The Wolf of Wall Street',
-        rating: 5,
-        body: "The result is a superb performance from DiCaprio as the swaggering, shouting bastard, and probably Scorsese's best film in 15 years."
-    }
-];
-
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res) => {
+  // 1
+  Review.find({}, (err, reviews) => {
+    if (err) {
+      console.log(err);
+    }
+
     res.render('reviews/index', {
-        reviews: reviews
+      reviews: reviews
     });
+  });
 });
+
 
 router.get('/reviews/new', (req, res) => {
     res.render('reviews/new');
